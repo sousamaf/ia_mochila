0c29ecb85eefc5143c5b596d907ac8a3855ec4e8
Marco Antonio Firmino de Sousa <marco.volare@gmail.com>
2013-02-25 22:19:18 -0300

    Revert "Metodo da Avaliacao."
    This reverts commit 788453d8e319bcbad0d1dae7a61ccfb066ce7b2e.

--- mochila/src/catolica/Mochila.java
+++ mochila/src/catolica/Mochila.java
@@ -8,11 +8,9 @@ import java.util.Random;
 
 public class Mochila {
 	
-	// Genes
+	
 	private static final int LIVROS = 6;
-	// Cromossomos
 	private static final int POPULACAO = 100;
-	// Carga maxima da mochila
 	private static final double MOCHILA = 2.0;
 	
 	// semente para o random
@@ -56,37 +54,6 @@ public class Mochila {
 	}
 	
 	/*
-	 * Metodo de avaliacao do cromossomo. Fitness function.
-	 */
-	public void avaliacao()
-	{
-		float peso = 0;
-		float peso_total = 0;
-		int i, j;
-		// Faz o calculo do peso de cada individuo
-		for(i = 0; i < POPULACAO; i++)
-		{
-			for(j = 0; j < LIVROS; j++)
-			{
-				if(pop[i][j] == true)
-					peso += pesos_livros[j];
-			}
-			aptidao[i][0] = (float) (peso > MOCHILA ? 0 : peso);
-			peso_total += peso; 
-			peso = 0;
-		}
-		// Faz o calculo da porcentagem para a roleta de cada individuo
-		for(i = 0; i < POPULACAO; i++)
-		{
-			aptidao[i][1] = (aptidao[i][0] * 100)/peso_total;
-		}
-	}
-	public int roleta()
-	{
-		return 0;
-	}
-	
-	/*
 	 * Metodo para carregamento dos valores dos pesos dos livros
 	 * a partir de um arquivo.
 	 */
@@ -118,7 +85,7 @@ public class Mochila {
 		ag.populacao_inicial();
 		ag.mutacao();
 		ag.carrega_pesos();
-		ag.avaliacao();
+
 	}
 
 }
